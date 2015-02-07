4fbd6428da9b7b8c192468738b64ebad8f8408a1
manjunadh <manjunadhb@gmail.com>
2015-02-07 13:22:03 +0530

    committing changes
    committing changes

---
+++ NCA/NCA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NCA
+//
+//  Created by Manjunadh on 07/02/15.
+//  Copyright (c) 2015 BRN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NCA/NCATests/NCATests.m
@@ -0,0 +1,34 @@
+//
+//  NCATests.m
+//  NCATests
+//
+//  Created by Manjunadh on 07/02/15.
+//  Copyright (c) 2015 BRN. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NCATests : XCTestCase
+
+@end
+
+@implementation NCATests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
